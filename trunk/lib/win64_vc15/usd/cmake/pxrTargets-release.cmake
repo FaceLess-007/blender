d538a570b0c5308a6dc043bde681a13e3f3aa990
LazyDodo <LazyDodo@954f8c5b-7b00-dc11-b283-0030488c597c>
2019-11-23 01:12:31 +0000

    windows: provisionary usd 19.11 libs

---
+++ trunk/lib/win64_vc15/usd/cmake/pxrTargets-release.cmake
@@ -0,0 +1,259 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "arch" for configuration "Release"
+set_property(TARGET arch APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(arch PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libarch.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS arch )
+list(APPEND _IMPORT_CHECK_FILES_FOR_arch "${_IMPORT_PREFIX}/lib/libarch.lib" )
+
+# Import target "tf" for configuration "Release"
+set_property(TARGET tf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(tf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libtf.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS tf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_tf "${_IMPORT_PREFIX}/lib/libtf.lib" )
+
+# Import target "gf" for configuration "Release"
+set_property(TARGET gf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(gf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libgf.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS gf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_gf "${_IMPORT_PREFIX}/lib/libgf.lib" )
+
+# Import target "js" for configuration "Release"
+set_property(TARGET js APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(js PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libjs.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS js )
+list(APPEND _IMPORT_CHECK_FILES_FOR_js "${_IMPORT_PREFIX}/lib/libjs.lib" )
+
+# Import target "trace" for configuration "Release"
+set_property(TARGET trace APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(trace PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libtrace.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS trace )
+list(APPEND _IMPORT_CHECK_FILES_FOR_trace "${_IMPORT_PREFIX}/lib/libtrace.lib" )
+
+# Import target "work" for configuration "Release"
+set_property(TARGET work APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(work PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libwork.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS work )
+list(APPEND _IMPORT_CHECK_FILES_FOR_work "${_IMPORT_PREFIX}/lib/libwork.lib" )
+
+# Import target "plug" for configuration "Release"
+set_property(TARGET plug APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(plug PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libplug.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS plug )
+list(APPEND _IMPORT_CHECK_FILES_FOR_plug "${_IMPORT_PREFIX}/lib/libplug.lib" )
+
+# Import target "vt" for configuration "Release"
+set_property(TARGET vt APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(vt PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libvt.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS vt )
+list(APPEND _IMPORT_CHECK_FILES_FOR_vt "${_IMPORT_PREFIX}/lib/libvt.lib" )
+
+# Import target "ar" for configuration "Release"
+set_property(TARGET ar APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ar PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libar.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ar )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ar "${_IMPORT_PREFIX}/lib/libar.lib" )
+
+# Import target "kind" for configuration "Release"
+set_property(TARGET kind APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(kind PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libkind.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS kind )
+list(APPEND _IMPORT_CHECK_FILES_FOR_kind "${_IMPORT_PREFIX}/lib/libkind.lib" )
+
+# Import target "sdf" for configuration "Release"
+set_property(TARGET sdf APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(sdf PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libsdf.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS sdf )
+list(APPEND _IMPORT_CHECK_FILES_FOR_sdf "${_IMPORT_PREFIX}/lib/libsdf.lib" )
+
+# Import target "ndr" for configuration "Release"
+set_property(TARGET ndr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(ndr PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libndr.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS ndr )
+list(APPEND _IMPORT_CHECK_FILES_FOR_ndr "${_IMPORT_PREFIX}/lib/libndr.lib" )
+
+# Import target "sdr" for configuration "Release"
+set_property(TARGET sdr APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(sdr PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libsdr.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS sdr )
+list(APPEND _IMPORT_CHECK_FILES_FOR_sdr "${_IMPORT_PREFIX}/lib/libsdr.lib" )
+
+# Import target "pcp" for configuration "Release"
+set_property(TARGET pcp APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(pcp PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libpcp.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS pcp )
+list(APPEND _IMPORT_CHECK_FILES_FOR_pcp "${_IMPORT_PREFIX}/lib/libpcp.lib" )
+
+# Import target "usd" for configuration "Release"
+set_property(TARGET usd APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usd PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusd.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usd )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usd "${_IMPORT_PREFIX}/lib/libusd.lib" )
+
+# Import target "usdGeom" for configuration "Release"
+set_property(TARGET usdGeom APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdGeom PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdGeom.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdGeom )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdGeom "${_IMPORT_PREFIX}/lib/libusdGeom.lib" )
+
+# Import target "usdVol" for configuration "Release"
+set_property(TARGET usdVol APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdVol PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdVol.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdVol )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdVol "${_IMPORT_PREFIX}/lib/libusdVol.lib" )
+
+# Import target "usdLux" for configuration "Release"
+set_property(TARGET usdLux APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdLux PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdLux.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdLux )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdLux "${_IMPORT_PREFIX}/lib/libusdLux.lib" )
+
+# Import target "usdShade" for configuration "Release"
+set_property(TARGET usdShade APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdShade PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdShade.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdShade )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdShade "${_IMPORT_PREFIX}/lib/libusdShade.lib" )
+
+# Import target "usdRender" for configuration "Release"
+set_property(TARGET usdRender APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdRender PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdRender.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdRender )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdRender "${_IMPORT_PREFIX}/lib/libusdRender.lib" )
+
+# Import target "usdHydra" for configuration "Release"
+set_property(TARGET usdHydra APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdHydra PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdHydra.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdHydra )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdHydra "${_IMPORT_PREFIX}/lib/libusdHydra.lib" )
+
+# Import target "usdRi" for configuration "Release"
+set_property(TARGET usdRi APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdRi PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdRi.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdRi )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdRi "${_IMPORT_PREFIX}/lib/libusdRi.lib" )
+
+# Import target "usdSkel" for configuration "Release"
+set_property(TARGET usdSkel APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdSkel PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdSkel.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdSkel )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdSkel "${_IMPORT_PREFIX}/lib/libusdSkel.lib" )
+
+# Import target "usdUI" for configuration "Release"
+set_property(TARGET usdUI APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdUI PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdUI.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdUI )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdUI "${_IMPORT_PREFIX}/lib/libusdUI.lib" )
+
+# Import target "usdUtils" for configuration "Release"
+set_property(TARGET usdUtils APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(usdUtils PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libusdUtils.lib"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS usdUtils )
+list(APPEND _IMPORT_CHECK_FILES_FOR_usdUtils "${_IMPORT_PREFIX}/lib/libusdUtils.lib" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
